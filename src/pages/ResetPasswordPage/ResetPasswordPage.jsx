09b7f527e5ed6e3560000851f175e2247ae2064a
GhostKato <osadchenko.maksym@gmail.com>
2024-10-12 13:09:51 +0300

    commit7

--- src/pages/ResetPasswordPage/ResetPasswordPage.jsx
+++ src/pages/ResetPasswordPage/ResetPasswordPage.jsx
@@ -13,7 +13,7 @@ const ResetPasswordPage = () => {
     const newPassword = e.target.newPassword.value;
 
     try {
-      const response = await fetch('https://nodejs-hw-mongodb-nls0.onrender.com//reset-pwd', {
+      const response = await fetch('https://nodejs-hw-mongodb-nls0.onrender.com/auth/reset-pwd', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
