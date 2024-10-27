7e4070f23a21038434832f82144b76009724300f
GhostKato <osadchenko.maksym@gmail.com>
2024-10-27 19:10:04 +0200

    fixed fetch

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -2,29 +2,30 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import { contactsApi } from "../../config/contactsApi";
 
 export const register = createAsyncThunk(
-    'auth/register',
-    async (credentials, thunkApi) => {
-        try {
-            const { data } = await contactsApi.post('/auth/users/signup', credentials);            
-            return data;
-        } catch (err) {
-            return thunkApi.rejectWithValue(err.message);
-        }
+  'auth/register',
+  async ({ name, email, password }, { rejectWithValue }) => {
+    try {
+      const { data } = await contactsApi.post('/auth/register', { name, email, password });
+      console.log('Registration successful:', data);     
+      return data;
+    } catch (err) {
+      return rejectWithValue(err.response?.data?.message || 'Registration failed');
     }
+  }
 );
 
 export const logIn = createAsyncThunk(
   'auth/logIn',
   async ({ email, password }, { rejectWithValue }) => {
     try {
-      const response = await contactsApi.post('/auth/login', { email, password });
+      const response = await contactsApi.post('/auth/login', { email, password }, { withCredentials: true });
+      console.log('Login successful:', response.data);      
       return response.data;
     } catch (error) {
       return rejectWithValue(error.response?.data?.message || 'Login failed');
     }
   }
 );
-
 export const sendResetEmail = createAsyncThunk(
   'auth/sendResetEmail',
   async (email, { rejectWithValue }) => {
@@ -45,6 +46,31 @@ export const resetPassword = createAsyncThunk(
       return response.data;
     } catch (error) {
       return rejectWithValue(error.response?.data?.message || 'Could not reset password');
+    }
+  }
+);
+
+export const logOut = createAsyncThunk(
+  'auth/logOut',
+  async (_, { rejectWithValue }) => {
+    try {
+      await contactsApi.post('/auth/logout');
+     
+    } catch (error) {
+      return rejectWithValue(error.response?.data?.message || 'Logout failed');
+    }
+  }
+);
+
+export const refreshToken = createAsyncThunk(
+  'auth/refreshToken',
+  async (refreshToken, { rejectWithValue }) => {
+    try {
+      const response = await contactsApi.post('/auth/refresh-token', { token: refreshToken });    
+      
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(error.response?.data?.message || 'Unable to refresh token');
     }
   }
 );
