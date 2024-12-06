c04c7939319daf48af3d088b481a2391c86c17c2
GhostKato <osadchenko.maksym@gmail.com>
2024-12-06 12:38:10 +0200

    add-img-fixed-css

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,8 +1,12 @@
 import s from './HomePage.module.css'
+import phonebookImg from '../../img/phonebook.jpg';
 
 const HomePage = () => {
   return (
-    <div className={s.container}><h1 className={s.title}>Welcome to the phonebook.</h1></div>
+    <div className={s.container}>
+      <h1 className={s.title}>Welcome to the phonebook.</h1>
+       <img className={s.img} src={phonebookImg} alt="Phonebook image" />
+    </div>
   )
 }
 
