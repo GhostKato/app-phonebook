8643dcc665e3c184f1f9372a05ff5c30296bd740
GhostKato <osadchenko.maksym@gmail.com>
2024-12-08 01:41:03 +0200

    edit

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -2,6 +2,7 @@ import { useDispatch, useSelector } from 'react-redux'
 import s from './UserMenu.module.css'
 import { selectUser } from '../../redux/auth/selectors'
 import { logOut } from '../../redux/auth/operations';
+import useResponsiveName from '../../hooks/useResponsiveName';
 
 const UserMenu = () => {  
 
@@ -9,9 +10,11 @@ const UserMenu = () => {
 
   const user = useSelector(selectUser);
 
+  const responsiveName = useResponsiveName(user.name);
+
   return (
     <div className={s.container}>      
-      <p> {user.name}</p>                          
+      <p title={user.name}> {responsiveName}</p>                          
       <button onClick={() => dispatch(logOut())} className={s.btn} type='button'>logout</button>
     </div>
   )
---
+++ src/hooks/useResponsiveEmail.js
@@ -0,0 +1,29 @@
+import { useState, useEffect } from 'react';
+
+const useResponsiveEmail = (email, maxLength = 20) => {
+  const [responsiveEmail, setResponsiveEmail] = useState(email);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth <= 405) {
+        setResponsiveEmail(
+          email.length > maxLength ? `${email.slice(0, maxLength)}...` : email
+        );
+      } else {
+        setResponsiveEmail(email);
+      }
+    };
+    
+    handleResize();
+    
+    window.addEventListener('resize', handleResize);
+    
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [email, maxLength]);
+
+  return responsiveEmail;
+};
+
+export default useResponsiveEmail;
