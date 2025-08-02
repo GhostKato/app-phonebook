8a02f942cea9b0fcc17db4e57ac12a05f55508e5
Maksym <154093363+GhostKato@users.noreply.github.com>
2025-08-02 18:39:58 +0300

    Update toastMessages.js

--- src/constants/toastMessages.js
+++ src/constants/toastMessages.js
@@ -3,7 +3,7 @@ export const MESSAGES = {
     ERROR: "Email is already in use. Try another mail!",
   },
   LOGIN: {
-    SUCCESS: "Welcome to TaskPro! 🚀",
+    SUCCESS: "Welcome to App-Phonebook! 🚀",
     ERROR: "Incorrect email or password. Please try again.",
   },
   SEND_RESET_EMAIL: {
